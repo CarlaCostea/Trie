ce84dca8ed03b82e19f5efcee1bd20db8ba1adc3
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-06-01 19:40:40 +0300

    Initial commit

---
+++ TrieAutocomplete/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrieAutocomplete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TrieAutocompleteTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TrieAutocompleteTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
