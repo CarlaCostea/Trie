b479a7d0e2cd3eef22049c1787e141828b7cfb08
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-06-03 14:21:17 +0300

    trie

---
+++ TrieAutocomplete/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrieAutocomplete
+{
+    public class Node
+    {
+        public Node[] Children = new Node[AlphabetSize];
+        const int AlphabetSize = 26;
+        private bool endOfWord;
+
+        public Node()
+        {
+            endOfWord = false;
+            for (int i = 0; i < AlphabetSize; i++)
+            {
+                Children[i] = null;
+            }
+        }
+
+        public Node GetNode()
+        {
+            return new Node();
+        }
+    }
+}
---
+++ TrieAutocomplete/Trie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrieAutocomplete
+{
+    public class Trie
+    {
+        private Node root;
+
+        public Trie()
+        {
+            root = root.GetNode();
+        }
+
+        public void Add(string word)
+        {
+            if (word == null)
+            {
+                return;
+            }
+
+            int alphabetIndex;
+            Node parrent = root;
+            for (int letterIndex = 0; letterIndex < word.Length; letterIndex++)
+            {
+                alphabetIndex = 'a' - word[letterIndex];
+                if (alphabetIndex < 0)
+                {
+                    break;
+                }
+
+                parrent.Children[alphabetIndex] ??= parrent.GetNode();
+
+                parrent = parrent.Children[alphabetIndex];
+            }
+
+            parrent.EndOfWord = true;
+        }
+    }
+}
