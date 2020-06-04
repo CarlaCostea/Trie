d63bcc13606ad7c860b109b904f3a362ea5d349b
CarlaCostea <CarlaCostea@users.noreply.github.com>
2020-06-04 14:15:52 +0300

    all green

--- TrieAutocompleteTests/TrieTests.cs
+++ TrieAutocompleteTests/TrieTests.cs
@@ -9,7 +9,7 @@ namespace TrieAutocompleteTests
     public class TrieTests
     {
         [Fact]
-        public void TestExample()
+        public void IfWeHaveSuggestionsForGivenStringWeShouldTypeThem()
         {
             Trie myTrie = new Trie();
             myTrie.Add("hell");
@@ -18,6 +18,48 @@ namespace TrieAutocompleteTests
 
             var expected = new string[] { "hell", "hello" };
             var result = myTrie.GetWords("hel");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void IfWeDoNotHaveSuggestionsForGivenStringWeShouldReturnAnEmptyString()
+        {
+            Trie myTrie = new Trie();
+            myTrie.Add("hell");
+            myTrie.Add("hello");
+            myTrie.Add("bye");
+
+            var expected = new string[] {};
+            var result = myTrie.GetWords("noSuggestion");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void IfTheGivenStringIsEmptyWeShouldReturnAllTheWordsFromTrie()
+        {
+            Trie myTrie = new Trie();
+            myTrie.Add("hell");
+            myTrie.Add("hello");
+            myTrie.Add("bye");
+
+            var expected = new string[] { "hell", "hello", "bye" };
+            var result = myTrie.GetWords("");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void IfTheGivenStringMatchesAWordFromTreeWeShouldReturnIt()
+        {
+            Trie myTrie = new Trie();
+            myTrie.Add("hell");
+            myTrie.Add("hello");
+            myTrie.Add("bye");
+
+            var expected = new string[] { "hell", "hello" };
+            var result = myTrie.GetWords("hell");
 
             Assert.Equal(expected, result);
         }
